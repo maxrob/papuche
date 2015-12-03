1152190f2d0327a7c64ce52ece6907363bf183f0
Maxime Robin <robin.maxime@netcourrier.com>
2015-12-03 16:10:06 +0100

    Add function model Story

--- app/models/story.rb
+++ app/models/story.rb
@@ -9,8 +9,24 @@ class Story < ActiveRecord::Base
       minimum: 1,
       maximum: 10,
       tokenizer: lambda { |str| str.scan(/\s+|$/) },
-  },
-  #TODO vérifier limite par nom et pas par caractères
-  
+  }
+
+  # TODO vérifier limite par nom et pas par caractères
+
+  def finished!
+    self.finished = true
+  end
+
+  def self.all_finished
+    Story.where(finished: true).includes(:messages, :user)
+  end
+
+  def self.all_unfinished
+    Story.where(finished: false).includes(:messages, :user)
+  end
+
+  def self.random
+    Story.all_finished.shuffle
+  end
 
 end
