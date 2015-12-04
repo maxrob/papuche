534002e321258c0790394148367af40d0c54951d
Bernard Vong <vong.bernard@gmail.com>
2015-12-04 10:12:19 +0100

    Force session for listing stories & add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,22 @@
 Rails.application.routes.draw do
+
+  # Devise g users
   devise_for :users
+
+
+  root 'stories#index'
+
+  resources :stories
+  get 'stories' => 'stories#index'
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  #root 'stories#index'
-
-  # Example of regular route:
-  #get 'stories/index' => 'stories#index'
-
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
 
   # Example resource route with options:
   #   resources :products do
