254d9b2a23b59487049c40b68f6250effae93d71
Maxime Robin <robin.maxime@netcourrier.com>
2015-12-13 22:43:12 +0100

    get stories of user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      @user = User.find(params[:id]).includes(:stories, :messages)
     end
 
 end
