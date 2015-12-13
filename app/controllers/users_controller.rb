50db378e8a6760894f37e8d4cf0d67580b34041a
Maxime Robin <robin.maxime@netcourrier.com>
2015-12-13 23:30:37 +0100

    Update user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,8 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.where(id: params[:id]).includes(:stories, :messages)
+      @user = User.find(params[:id])
+      @stories = Story.where(user_id: params[:id]).includes(:messages, :user, :likes)
     end
 
 end
