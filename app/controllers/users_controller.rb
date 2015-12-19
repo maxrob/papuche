05e5b8b141f6613ef646f0fae0fc7d4537c08171
Bernard Vong <vong.bernard@gmail.com>
2015-12-20 00:54:59 +0100

    Remove changes on user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
 
   # GET /users
   def index
@@ -8,15 +7,9 @@ class UsersController < ApplicationController
 
   # GET /users/1
   def show
+    @user = User.find(params[:id])
     @liked_stories = Story.all_liked(user_id: params[:id]).page(params[:page])
     @contributed_stories = Story.all_contributed(user_id: params[:id]).page(params[:page])
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-      @stories = Story.where(user_id: params[:id]).includes(:messages, :user, :likes).page params[:page]
-    end
 
 end
